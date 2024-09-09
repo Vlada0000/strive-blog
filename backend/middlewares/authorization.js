14da060600b6b9f37c525b84315f75e88c1b85b5
Vlada0000 <161520314+Vlada0000@users.noreply.github.com>
2024-09-09 12:24:24 +0200

    Update authorization.js

--- backend/middlewares/authorization.js
+++ backend/middlewares/authorization.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 import Author from '../models/Authors.js';
 
 export default async (req, res, next) => {
-    // Verifica se l'header Authorization è presente e ha il formato corretto
+    
     if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
         return res.status(401).json({ error: 'Missing or invalid Authorization header' });
     }
@@ -10,25 +10,25 @@ export default async (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
 
     try {
-        // Verifica e decodifica il token JWT
+        
         const payload = jwt.verify(token, process.env.JWT_SECRET);
 
-        // Recupera i dati dell'utente dal database
-        const author = await Author.findById(payload.authorId); //.select('-password');
+        
+        const author = await Author.findById(payload.authorId); 
 
-        // Verifica se l'autore esiste
+        
         if (!author) {
             return res.status(401).json({ error: 'User not found' });
         }
 
-        // Aggiunge l'utente autenticato all'oggetto `req`
+        
         req.loggedAuthor = author;
         console.log('Authenticated user:', author);
 
-        // Passa al prossimo middleware
+       
         next();
     } catch (err) {
-        // Gestione degli errori del token
+        
         if (err.name === 'JsonWebTokenError') {
             return res.status(401).json({ error: 'Invalid token' });
         }
